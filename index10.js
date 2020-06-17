4dd328ff2e42b47498f3e07aa9f50b7cf17647c3
sarah845 <63765857+sarah845@users.noreply.github.com>
2020-06-16 23:29:47 -0700

    Update index10.js

--- index10.js
+++ index10.js
@@ -4,13 +4,19 @@ var router = express.Router();
 // our constructor
 function Note(pTitle, pDay, pTime,pStatut) 
 {this.title= pTitle; this.day = pDay; this.time = pTime; 
- this.statut=pStatut; this.toString= function(){return this.title+" due on "+this.day+" at "+this.time+"<br>"}}
+this.statut=pStatut; };
+Note.prototype.toString = function () 
+{return this.title+" due on "+ this.day+ " at "+ this.time+"<br>";};
 
-ServerNotes = [];
+ServerNotes = [];ServerNotess=[];ServerNotesn=[];ServerNotesd=[];
 // save typing time, make up 3 for testing
 ServerNotes.push(new Note("Eng101", "Moonday 5/18","11:59 pm", "d"));
  ServerNotes.push(new Note("Prog209", "Monday 5/18", "11:59 pm","n"));
  ServerNotes.push(new Note("Play vid game", "Saturday 7/23", "10:30 am","s"));
+ 
+ let ServerNotesd = ServerNotes.filter(item => item.statut == "d");
+	let ServerNotess = ServerNotes.filter(item => item.statut == "s");
+	let ServerNotesn = ServerNotes.filter(item => item.statut == "n");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,6 +27,18 @@ router.get('/', function(req, res, next) {
 /* GET All Notes data */
 router.get('/getAllNotes', function(req, res) {
   res.status(200).json(ServerNotes);
+});
+
+router.get('/getdNotes', function(req, res) {
+  res.status(200).json(ServerNotesd);
+});
+
+router.get('/getsNotes', function(req, res) {
+  res.status(200).json(ServerNotess);
+});
+
+router.get('/getnNotes', function(req, res) {
+  res.status(200).json(ServerNotesn);
 });
 
 
@@ -56,28 +74,5 @@ router.delete('/DeleteNote/:title', (req, res) => {
   }
 });
 
-router.Edit('/Editnote/:title', (req, res) => {
-  const title = req.params.title;
-  let found = false;
-  console.log(title);    
-
-  for(var i = 0; i < ServerNotes.length; i++) // find the match
-  {
-      if(ServerNotes[i].title === title){
-        ServerNotes.splice(i,1);  // remove object from array
-          found = true;
-          break;
-      }
-  }
-
-  if (!found) {
-    console.log("not found");
-    return res.status(500).json({
-      status: "error"
-    });
-  } else {
-  res.send('Note ' + title + ' deleted!');
-  }
-});
 
 module.exports = router;
